60e5c424309ca3b629d6b8d71bdf8a463aa53749
INAFF25252 <113192898+INAFF25252@users.noreply.github.com>
2023-10-26 19:27:49 -0400

    lkbl

--- index.js
+++ index.js
@@ -1,120 +1,20 @@
-const books = [
-  {
-    name: "To Kill a Mockingbird",
-    authorFirst: "Harper",
-    authorLast: "Lee",
-    publishDate: 1960,
-    genre: ["mystery", "historical", "fiction"],
-  },
-  {
-    name: "The Great Gatsby",
-    authorFirst: "F. Scott",
-    authorLast: "Fitzgerald",
-    publishDate: 1925,
-    genre: ["mystery", "historical", "fiction"],
-  },
-  {
-    name: "The Hobbit",
-    authorFirst: "JRR",
-    authorLast: "Tolkien",
-    publishDate: 1937,
-    genre: ["fantasy", "fiction"],
-  },
-  {
-    name: "Harry Potter and the Deathly Hallows",
-    authorFirst: "JK",
-    authorLast: "Rowling",
-    publishDate: 1997,
-    genre: ["fantasy", , "fiction"],
-  },
-  {
-    name: "Ulysses",
-    authorFirst: "James",
-    authorLast: "Joyce",
-    publishDate: 1918,
-    genre: ["historical", "non-fiction"],
-  },
-  {
-    name: "War and Peace",
-    authorFirst: "Leo",
-    authorLast: "Tolstoy",
-    publishDate: 1867,
-    genre: ["historical", "fiction"],
-  },
-  {
-    name: "Pride and Prejudice",
-    authorFirst: "Jane",
-    authorLast: "Austen",
-    publishDate: 1813,
-    genre: ["fiction"],
-  },
-  {
-    name: "The Catcher in the Rye",
-    authorFirst: "JD",
-    authorLast: "Salinger",
-    publishDate: 1951,
-    genre: ["fiction"],
-  },
-  {
-    name: "Great Expectations",
-    authorFirst: "Charles",
-    authorLast: "Dickens",
-    publishDate: 1861,
-    genre: ["historical", "fiction"],
-  },
-  {
-    name: "Little Women",
-    authorFirst: "Lousia May",
-    authorLast: "Alcott",
-    publishDate: 1868,
-    genre: ["womens", "historical", "fiction"],
-  },
-];
-
-const titles = [
-  "To Kill a Mockingbird",
-  "The Great Gatsby",
-  "The Hobbit",
-  "Harry Potter and the Deathly Hallows",
-  "Ulysses",
-  "War and Peace",
-  "Pride and Prejudice",
-  "The Catcher in the Rye",
-  "Great Expectations",
-  "Little Women",
-];
-
-//Array of authors and the book they wrote
-//"--- wrote --- in ---"
-books.forEach((book)=> console.log(`${book.authorFirst} ${book.authorLast} wrote ${book.name}`))
-//sort books alphabetically
-const abc = titles.sort((book1, book2)=> {
-  const nameA = book1.toUpperCase(); // ignore upper and lowercase
-  const nameB = book2.toUpperCase(); // ignore upper and lowercase
-  if (nameA < nameB) {
-    return -1;
-  }
-  if (nameA > nameB) {
-    return 1;
-  }
-
-  // names must be equal
-  return 0;
-});
-console.log(abc)
-//Find who wrote War and Peace
-const War = books.find((author)=>author.name === "War and Peace")
-console.log(`${War.authorFirst} ${War.authorLast} wrote War and Peace`)
-//how many books were written before 1900?
-const before = books.filter((date)=> date.publishDate < 1900)
-console.log(before.length)
-//was there at least one book published within the last 100 years?
-const wasthere = books.some((within)=> new Date().getFullYear() - within.publishDate < 100) 
-console.log(wasthere)
-//was every book published within the last 100 years?
-const every = books.every((within)=> new Date().getFullYear() - within.publishDate < 100) 
-console.log(every)
+const DOMSelectors = {
+  form:document.querySelector("#form"),
+  pokemon: document.querySelector(".pokemon-name"),
+  type: document.querySelector(".ele-type"),
+  img: document.querySelector(".image"),
+  input: document.querySelector(".input"),
+  gallery: document.querySelector(".gallery"),
+  h2: document.querySelector("h2")
+  };
   
-//print a list of books that "includes" the genre historical
-const his = books.filter((genre)=> genre.genre.includes("historical") )
-console.log(his)
+  
+  console.log(DOMSelectors);
+  DOMSelectors.form.addEventListener("submit", function(event){
+    event.preventDefault();
+    console.log(DOMSelectors.pokemon.value, DOMSelectors.type.value, DOMSelectors.img.value)
+    function card (){
+      DOMSelectors.gallery.insertAdjacentHTML("afterbegin", `<div class="card"> <h2> ${DOMSelectors.pokemon.value}</h2> </div>`)
+  
+    };
+    card()})
